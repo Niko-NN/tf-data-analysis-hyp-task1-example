c7bec7106b19388b855c42e933747d68b599e532
Nick <117512008+Niko-NN@users.noreply.github.com>
2023-04-13 22:19:55 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,16 @@
 import pandas as pd
 import numpy as np
+from scipy.stats import norm
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 5459656416 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    x_cr = x_success / x_cnt
+    y_cr = y_success / y_cnt
+    uplift = (y_cr - x_cr) / x_cr
+    z_score = uplift / (1.0 / y_cnt + 1.0 / x_cnt)**0.5
+    p_value = norm.sf(abs(z_score))
+    return p_value < 0.05
